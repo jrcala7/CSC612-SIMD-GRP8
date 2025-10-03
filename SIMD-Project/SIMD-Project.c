ca870a477158e2066c05827708c01e8eb05a0a19
JR Cala <jrcala7@gmail.com>
2025-10-03 18:21:01 +0800

    Vector Sum in C

--- SIMD-Project/SIMD-Project.c
+++ SIMD-Project/SIMD-Project.c
@@ -5,15 +5,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <math.h>
 
 extern void ymm_vector_add();
 extern void xmm_vector_add();
 extern void x86_vector_add();
 
+//Length of the vector
+#define VECTOR_LEN 8
+
+//Vector Sum Double and Len
+double VectorSum(double A[], int len) {
+	double ret = 0.0;
+	for (int i = 0; i < len; i++) {
+		ret += abs(A[i]);
+	}
+	return ret;
+}
+
 int main(int argc, char* argv[]) {
+	//Vector array to create
+	double vec[VECTOR_LEN];
+
+	//Creates the vector to pass
+	for (int i = 0; i < VECTOR_LEN; i++) {
+		vec[i] = sin((double)i * 0.0003) * cos((double)i * 0.0007) * 1000.0;
+		printf("Vector[%u]: %f\n", i, vec[i]);
+	}
+
+	double c_sum = VectorSum(vec, VECTOR_LEN);
+	printf("Vec sum in C: %f\n", c_sum);
+
 	ymm_vector_add();
 	xmm_vector_add();
 	x86_vector_add();
+
 	return 0;
 }
 
